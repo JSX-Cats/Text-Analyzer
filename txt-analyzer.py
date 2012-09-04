79cad5f940499d2d7bd9826d9d4a7a2f508fb65e
punkdbaba <yashwanth34@gmail.com>
2012-09-04 15:34:09 +0530

    ^V v v v v v v v vvv
    v
    ^t

---
+++ txt-analyzer.py
@@ -0,0 +1,39 @@
+####################################################################
+###############################::INFO::#############################
+# 
+# All rights reserved to Yashwanth Maheshwaram at Braceat, Inc.
+# This is a code to analyze as small piece of text that is written
+# onto sample.txt. This script can analyze
+# No. of words, 
+# No. of word repetetions
+# No. of Adjectives, Adverbs, Nouns and Verbs Syllable wise
+# 
+
+import sys, re, os
+
+def main():
+  f = fopen ( sample.txt, 'r')
+  buff = f.read()
+  adjs = [] #Empty list for appending Adjectives
+  advrbs = []
+  vrbs = []
+  nouns = []
+
+  
+def adjects():
+  f1 = fopen(r'adjectives/28Kadjectives.txt', 'r')
+  adjs = f1.read()
+  for buff in adjs
+    adjs.append(buff)
+"""
+def adverbs():
+  f2 = fopen(r'adverbs//6kadverbs.txt', 'r')
+  advrbs = f2.read()
+  for buff in advrbs
+    advrbs.append(buff)
+
+"""
+if __name__ == '__main__':
+  main()
+
+
